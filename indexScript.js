205b44cb6829347a1aa29f13e6b78024d0ad9b84
Graer Sultan Styne <graer.styne46@stu.bryanisd.org>
2024-11-26 23:24:42 -0600

    Create indexScript.js

---
+++ indexScript.js
@@ -0,0 +1,26 @@
+function toggleMenu()
+{
+    var menu = document.querySelector(".menu");
+    var menuButton = document.querySelector(".menuButton");
+    if (menu.style.width === "50%")
+    {
+        menu.style.width = "50px";
+            
+        menuButton.style.position = "absolute"; //position: absolute;
+        menuButton.style.top = "50%"; //top: 50%;
+        menuButton.style.left = "50%"; //left: 50%;
+        menuButton.style.right = "auto";
+        menuButton.style.transform = "translate(-50%, -50%)"; //transform: translate(-50%, -50%);
+        menuButton.style.float = "none";
+    }
+    else
+    {
+        menu.style.width = "50%";
+            
+        menuButton.style.position = "absolute";
+        menuButton.style.top = "50%";
+        menuButton.style.left = "auto";
+        menuButton.style.right = "10px";
+        menuButton.style.transform = "translateY(-50%)";
+      }
+  }
